7943aa63ea0275459ccd5481417f376f35d24df4
Mr. Barfi <barfider@gmail.com>
2019-05-12 04:10:03 +0300

    Barfi edition

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-/**
- * JS here
+/* -----------------------------------------------------------------------------
+ * Java Script here
+ * -----------------------------------------------------------------------------
  */
+
+/*
+ * Smooth scroll plugin
+ * Docs: https://github.com/cferdinandi/smooth-scroll
+ */
+var scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 500, // Скорость прокрутки
+  speedAsDuration: true, // Вне зависимости от расстояния будет крутить за одно время
+  header: '#page-header' // Селектор header сайта, так как он fixed, его высоту надо учитывать для отступа
+});
